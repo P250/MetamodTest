888caf184cf11998b06a432fd76ec8872a6ed4d9
root <lijschristian@gmail.com>
2025-06-21 22:14:03 +0100

    add flag so it compflag so it compiles on linux

--- linuxbuild/versioning/versionlib.cpp
+++ linuxbuild/versioning/versionlib.cpp
@@ -1,6 +1,6 @@
 
-const char *PLUGIN_FULL_VERSION   = "1.0.0.202f6c6";
-const char *PLUGIN_REVISION_SHORT = "202f6c6";
-const char *PLUGIN_REVISION_LONG  = "202f6c60ebd19dced97f19aa90fb5125877df1d8";
-const char *PLUGIN_REVISION_COUNT = "2";
+const char *PLUGIN_FULL_VERSION   = "1.0.0.931ef43";
+const char *PLUGIN_REVISION_SHORT = "931ef43";
+const char *PLUGIN_REVISION_LONG  = "931ef43b4618a1463418a0904c15c6795c28104f";
+const char *PLUGIN_REVISION_COUNT = "3";
     
